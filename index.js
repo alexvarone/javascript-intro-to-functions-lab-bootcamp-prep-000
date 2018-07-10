ef8fde18015041c484eb9c9fb2fcf355100f1143
Alex Varone <alexvarone@ymail.com>
2018-07-10 03:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,17 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-  return "I can\'t hear you.";
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
- } else if (string === "I love you, Grandma.") {
-   return "I love you, too.";
-}
-
-
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!";
@@ -33,4 +22,4 @@ function sayHiToGrandma(string) {
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
-};
+}
